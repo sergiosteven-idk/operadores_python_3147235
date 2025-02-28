bc00669481715fb8e17215a3e7a888df5a8c73a6
Santiago <santiagaoevllafonseca@gmail.com>
2025-02-28 09:42:05 -0500

    la opracion entre parentesis

--- operadores.py
+++ operadores.py
@@ -9,6 +9,7 @@ siguen  el siguente orden jerarquico:
 a = 3
 b = 2
 c = 1 
+x = 5 
 
 y =  a * b + c 
 print (y)  #7
@@ -21,4 +22,17 @@ y = a * b - 1 / 3 + c
 print (y) 
 
 y = a ** 2 - a * b - 1 / 3 + c 
+print (y) 
+
+y = a * 2 ** 3 - a + c -c ** 3 + c
+print (y) 
+
+'''
+NOTA:Si operaciones en el mismo  nivel si hay parentesisentro de parentesis primero en parentesis interno
+'''
+
+y = a ** 2 * 3 / (c - x )
+print (y) 
+
+y = ((2 * a + c )/ 7) * (a +(4 * a )/ c)
 print (y) 
