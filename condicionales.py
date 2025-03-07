e753fb3b3b85449a9e9c1e4092c9ede1cf83dc62
Fer <leonmafe788@gmail.com>
2025-03-07 10:34:01 -0500

    condicionales avanzados

---
+++ condicionales.py
@@ -0,0 +1,18 @@
+'''
+CONDICIONAL:
+Expresion que, al ser evaluada
+da como resultado un valor verdadero.
+por tanto, en una condicional debe haber
+operadores relacionales o logicos
+'''
+
+#Ejemplo de condicional
+
+a = 6
+b = 3
+c = 7
+d = 4
+e = 5
+
+resultado = ( a + b > c / d) or e * 2 != d + c and not (a < b)
+print(resultado)
